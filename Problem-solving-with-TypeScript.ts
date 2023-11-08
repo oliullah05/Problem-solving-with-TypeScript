6a5ddf961e5c45ce3a3bc291a8bf94a8abeddf04
oliullah05 <mdolihasan@gmail.com>
2023-11-08 16:55:55 +0600

    Problem-4 Solved

--- Problem-solving-with-TypeScript.ts
+++ Problem-solving-with-TypeScript.ts
@@ -106,6 +106,79 @@ else{
 
 
 
+/* Problem-4:
+
+   You got a list that has numbers and words mixed together. Your job is to make a function that will take the list as an argument and return the total by adding them up.
+
+   To solve this in TypeScript, you'll look at each thing in the list named mixedData, which looks like this: [1, 'two', 3, 'four', 5]. You'll go through the list, find the numbers, and add them together.
+
+   Every time you find a number in the list, you'll add it to a total. You'll start at zero and then keep adding the numbers you find. If no number is found in the list return 0. To make sure TypeScript knows these things are numbers, you'll use type assertions. */
+
+
+    // Solution-4
+
+
+type MixedData = (number|string )[];
+
+const mixedData:MixedData = [1, 'two', 3, 'four', 5,"six"]
+
+
+const findNumberAndSum =(arrey:MixedData):number=>{
+    let total = 0;
+for(let i =0;i<arrey.length;i++){
+    const index = arrey[i]
+
+    if( typeof index ==="number"){
+    total=total+index
+    }
+    
+}
+return total;
+}
+
+
+
+/*  Problem-5:
+
+       Define two interfaces: Car with properties like make, model, and year, and Driver with properties like name and licenseNumber. Create a function that takes two objects of type Car and Driver and returns an object with the combined properties of both types.*/
+
+
+    // Solution-5
+
+interface Car {
+    make:string,
+    model:string,
+    year:number
+}
+
+interface Driver{
+    name:"Oliullah",
+    licenseNumber:"WB008TS"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
